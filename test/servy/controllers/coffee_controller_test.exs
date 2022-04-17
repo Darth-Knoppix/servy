916fcb0639a42d8d6523d52515c7db700154be78
Seth Corker <seth.corker@gmail.com>
2022-04-17 13:45:12 +0100

    try rem whitespace

--- test/servy/controllers/coffee_controller_test.exs
+++ test/servy/controllers/coffee_controller_test.exs
@@ -2,36 +2,42 @@ defmodule ServyControllersCoffeeTest do
   use ExUnit.Case
   doctest Servy.Controllers.Coffee
 
+  defp remove_whitespace(text) do
+    String.replace(text, ~r{\s}, "")
+  end
+
   describe "index/1" do
     test "returns a valid result" do
-      body = """
-      <html>
-        <h1>All coffee orders</h1>
+      body =
+        remove_whitespace("""
+        <html>
+          <h1>All coffee orders</h1>
 
-        <ul>
+          <ul>
+              <li>Cappuccino with Soy</li>
+              <li>Espresso with no</li>
+              <li>Flat White with Cow</li>
+              <li>Latte with Almond</li>
+              <li>Ling Black with Cow</li>
+          </ul>
+        </html>
+        """)
 
-            <li>Cappuccino with Soy</li>
-
-            <li>Espresso with no</li>
-
-            <li>Flat White with Cow</li>
-
-            <li>Latte with Almond</li>
-
-            <li>Ling Black with Cow</li>
-
-        </ul>
-      </html>
-      """
-
-      response = %Servy.Request{
+      expected_response = %Servy.Request{
         response: %Servy.Response{
           body: body,
           status: 200
         }
       }
 
-      assert Servy.Controllers.Coffee.index(%Servy.Request{}) == response
+      result = Servy.Controllers.Coffee.index(%Servy.Request{})
+
+      result = %{
+        result
+        | response: %{result.response | body: remove_whitespace(result.response.body)}
+      }
+
+      assert result == expected_response
     end
   end
 
